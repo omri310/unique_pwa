14f77d05ac1678b4bae32b4bae5608d94938db26
omri310 <omrifrazer@gmail.com>
2024-04-12 09:38:57 -0700

    install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -11,6 +11,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
     if (!promptEvent) {
         return;
        }
